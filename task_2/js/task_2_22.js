11227ca4d428b4c96a4266430437fdd2333930ce
zhengss33 <493164072@qq.com>
2016-08-13 01:05:16 +0800

    Merge branch 'master' into gh-pages

--- task_2/js/task_2_22.js
+++ task_2/js/task_2_22.js
@@ -12,6 +12,26 @@
         }
     };
 
+    var inOrder = function(node){
+        if (node != null) {
+            var nodeItem = Array.prototype.slice.apply(node.childNodes);
+            inOrder(nodeItem[1]);
+            nodeList.push(node);
+            inOrder(nodeItem[nodeItem.length-2]);
+            document.getElementById('tip').innerHTML = '遍历中...';
+        }
+    };
+
+    var posOrder =function(node){
+        if (node != null) {
+            var nodeItem = Array.prototype.slice.apply(node.childNodes);
+            nodeList.unshift(node);
+            posOrder(nodeItem[1]);
+            posOrder(nodeItem[nodeItem.length-2]);
+            document.getElementById('tip').innerHTML = '遍历中...';
+        }
+    };
+
     var traverseColor = function(){
         var i = 1;
         nodeList[0].style.background = 'blue';
@@ -28,7 +48,20 @@
         },300);    
     };
     
-    document.getElementById('startbtn').onclick = function(){
+    document.getElementById('prebtn').onclick = function(){
+        nodeList = [];
         preOrder(node);
+        traverseColor();           
+    };
+
+    document.getElementById('posbtn').onclick = function(){
+        nodeList = [];
+        posOrder(node);
+        traverseColor();           
+    };
+
+    document.getElementById('inbtn').onclick = function(){
+        nodeList = [];
+        inOrder(node);
         traverseColor();           
     };
